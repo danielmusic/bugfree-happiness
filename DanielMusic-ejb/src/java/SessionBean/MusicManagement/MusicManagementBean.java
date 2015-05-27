89e5f81e1b6c4c0d06fd217d231afc1d2ea57aba
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2015-05-27 13:19:47 +0800

    Paypal fix

--- DanielMusic-ejb/src/java/SessionBean/MusicManagement/MusicManagementBean.java
+++ DanielMusic-ejb/src/java/SessionBean/MusicManagement/MusicManagementBean.java
@@ -53,42 +53,51 @@ public class MusicManagementBean implements MusicManagementBeanLocal {
     @Override
     public void testAdaptivePayment() {
         try {
-        PayRequest payRequest = new PayRequest();
+            PayRequest payRequest = new PayRequest();
 
-        List<Receiver> receivers = new ArrayList<Receiver>();
-//Artist
-        Receiver secondaryReceiver = new Receiver();
-        secondaryReceiver.setAmount(1.00);
-        secondaryReceiver.setEmail("platfo_1255170694_biz@gmail.com");
-        receivers.add(secondaryReceiver);
+            List<Receiver> receivers = new ArrayList<Receiver>();
+//Artist (partial of the total)
+            Receiver secondaryReceiver = new Receiver();
+            secondaryReceiver.setAmount(0.90);
+            secondaryReceiver.setEmail("daniel-artist@hotmail.com");
+            receivers.add(secondaryReceiver);
 
-//Daniel
-        Receiver primaryReceiver = new Receiver();
-        primaryReceiver.setAmount(2.00);
-        primaryReceiver.setEmail("platfo_1255612361_per@gmail.com");
-        primaryReceiver.setPrimary(true);
-        receivers.add(primaryReceiver);
+//Daniel (total amount)
+            Receiver primaryReceiver = new Receiver();
+            primaryReceiver.setAmount(1.00);
+            primaryReceiver.setEmail("danielmusic@hotmail.com");
+            primaryReceiver.setPrimary(true);
+            receivers.add(primaryReceiver);
 
-        ReceiverList receiverList = new ReceiverList(receivers);
-        payRequest.setReceiverList(receiverList);
+            ReceiverList receiverList = new ReceiverList(receivers);
+            payRequest.setReceiverList(receiverList);
 
-        RequestEnvelope requestEnvelope = new RequestEnvelope("en_US");
-        payRequest.setRequestEnvelope(requestEnvelope);
-        payRequest.setActionType("PAY");
-        payRequest.setCancelUrl("https://devtools-paypal.com/guide/ap_chained_payment?cancel=true");
-        payRequest.setReturnUrl("https://devtools-paypal.com/guide/ap_chained_payment?success=true");
-        payRequest.setCurrencyCode("USD");
-        payRequest.setIpnNotificationUrl("http://replaceIpnUrl.com");
+            RequestEnvelope requestEnvelope = new RequestEnvelope("en_US");
+            payRequest.setRequestEnvelope(requestEnvelope);
+            payRequest.setActionType("PAY");
+            payRequest.setCancelUrl("https://devtools-paypal.com/guide/ap_chained_payment?cancel=true");//Return if payment cancelled
+            payRequest.setReturnUrl("https://devtools-paypal.com/guide/ap_chained_payment?success=true");//Return after payment complete
+            payRequest.setCurrencyCode("USD");
+            payRequest.setIpnNotificationUrl("http://replaceIpnUrl.com");
 
-        Map<String, String> sdkConfig = new HashMap<String, String>();
-        sdkConfig.put("mode", "sandbox");
-        sdkConfig.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
-        sdkConfig.put("acct1.Password", "WX4WTU3S8MY44S7F");
-        sdkConfig.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
-        sdkConfig.put("acct1.AppId", "APP-80W284485P519543T");
+            Map<String, String> sdkConfig = new HashMap<String, String>();
+            sdkConfig.put("mode", "sandbox");
+            sdkConfig.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
+            sdkConfig.put("acct1.Password", "WX4WTU3S8MY44S7F");
+            sdkConfig.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
+            sdkConfig.put("acct1.AppId", "APP-80W284485P519543T");
 
-        AdaptivePaymentsService adaptivePaymentsService = new AdaptivePaymentsService(sdkConfig);
-        PayResponse payResponse = adaptivePaymentsService.pay(payRequest);
+            AdaptivePaymentsService adaptivePaymentsService = new AdaptivePaymentsService(sdkConfig);
+            PayResponse payResponse = adaptivePaymentsService.pay(payRequest);
+            
+            System.out.println("-----------");
+            System.out.println(payResponse.getPaymentExecStatus());
+            String payKey = payResponse.getPayKey();
+            System.out.println("Open this link in browser: https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&paykey="+payKey);
+            //open link in browser
+            //Accounts (all password is 12345678): 
+            //daniel-buyer@hotmail.com, daniel-artist@hotmail.com, danielmusic@hotmail.com
+            //Login at https://www.sandbox.paypal.com
         } catch (Exception ex) {
             ex.printStackTrace();
         }
