6a9b8a59d412339bcdeb7199f3ffcf9d1858fc14
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2015-06-05 19:20:50 +0800

    Approve & reject artist

--- DanielMusic-ejb/src/java/SessionBean/AdminManagement/AdminManagementBean.java
+++ DanielMusic-ejb/src/java/SessionBean/AdminManagement/AdminManagementBean.java
@@ -1,20 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SessionBean.AdminManagement;
 
-import SessionBean.AdminManagement.AdminManagementBeanLocal;
+import EntityManager.Account;
+import EntityManager.Artist;
+import EntityManager.ReturnHelper;
+import SessionBean.CommonInfrastructure.CommonInfrastructureBeanLocal;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-/**
- *
- * @author Jason
- */
 @Stateless
 public class AdminManagementBean implements AdminManagementBeanLocal {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    private static final String artistAccountApprovedSubject = "Daniel Music: Artist Account Approved";
+    private static final String artistAccountApprovedMsg = "Your artist account has been approved. Your artist profile, albums and tracks will now be shown to the public.";
+    private static final String artistAccountRejectedSubject = "Daniel Music: Artist Account Rejected";
+    private static final String artistAccountRejectedMsg = "Unfortuantely your artist account can not be approved.";
+
+    @EJB
+    private CommonInfrastructureBeanLocal cibl;
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public ReturnHelper approveArtist(Long artistID) {
+        System.out.println("AdminManagementBean: approveArtist() called");
+        ReturnHelper result = new ReturnHelper();
+        Query q = em.createQuery("SELECT s FROM Account s where s.id=:id");
+        q.setParameter("id", artistID);
+        try {
+            Account account = (Account) q.getSingleResult();
+            if (!(account instanceof Artist)) {
+                result.setResult(false);
+                result.setDescription("Account does not support this functionality. It is not an artist account.");
+            } else if (account.getIsDisabled() == true) {
+                result.setResult(false);
+                result.setDescription("Account can not be aprroved as it has been disabled.");
+            } else {
+                q = em.createQuery("SELECT s FROM Artist s where s.id=:id");
+                q.setParameter("id", artistID);
+                Artist artist = (Artist) q.getSingleResult();
+                artist.setIsApproved(true);
+                em.merge(artist);
+                result.setResult(true);
+                result.setDescription("Artist has been approved.");
+                cibl.sendEmail(artist.getEmail(), "TODO", artistAccountApprovedSubject, artistAccountApprovedMsg);
+            }
+        } catch (Exception ex) {
+            System.out.println("AdminManagementBean: approveArtist() failed");
+            result.setResult(false);
+            result.setDescription("Failed to approve artist. Internal server error.");
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public ReturnHelper rejectArtist(Long artistID) {
+        System.out.println("AdminManagementBean: rejectArtist() called");
+        ReturnHelper result = new ReturnHelper();
+        Query q = em.createQuery("SELECT s FROM Account s where s.id=:id");
+        q.setParameter("id", artistID);
+        try {
+            Account account = (Account) q.getSingleResult();
+            if (!(account instanceof Artist)) {
+                result.setResult(false);
+                result.setDescription("Account does not support this functionality. It is not an artist account.");
+            } else if (account.getIsDisabled() == true) {
+                result.setResult(false);
+                result.setDescription("Account can not be rejected as it has been disabled.");
+            } else {
+                q = em.createQuery("SELECT s FROM Artist s where s.id=:id");
+                q.setParameter("id", artistID);
+                Artist artist = (Artist) q.getSingleResult();
+                artist.setIsApproved(true);
+                em.merge(artist);
+                result.setResult(true);
+                result.setDescription("Artist has been rejected.");
+                cibl.sendEmail(artist.getEmail(), "TODO", artistAccountRejectedSubject, artistAccountRejectedMsg);
+            }
+        } catch (Exception ex) {
+            System.out.println("AdminManagementBean: rejectArtist() failed");
+            result.setResult(false);
+            result.setDescription("Failed to approve artist. Internal server error.");
+            ex.printStackTrace();
+        }
+        return result;
+    }
 }
--- DanielMusic-ejb/src/java/SessionBean/AdminManagement/AdminManagementBeanLocal.java
+++ DanielMusic-ejb/src/java/SessionBean/AdminManagement/AdminManagementBeanLocal.java
@@ -1,8 +1,11 @@
 package SessionBean.AdminManagement;
 
+import EntityManager.ReturnHelper;
 import javax.ejb.Local;
 
 @Local
 public interface AdminManagementBeanLocal {
-   
+    
+    public ReturnHelper approveArtist(Long artistID);
+    public ReturnHelper rejectArtist(Long artistID);
 }
