d3f9e8ff70954f6d4ef378af553dfb8df957bb4b
-VeRyLuNaTiC <-VeRyLuNaTiC@Juno>
2015-08-19 17:23:09 +0800

    reset back original online payment url

--- DanielMusic-ejb/src/java/SessionBean/ClientManagement/ClientManagementBean.java
+++ DanielMusic-ejb/src/java/SessionBean/ClientManagement/ClientManagementBean.java
@@ -273,10 +273,10 @@ public class ClientManagementBean implements ClientManagementBeanLocal {
             payRequest.setRequestEnvelope(requestEnvelope);
             payRequest.setActionType("PAY");
             payRequest.setFeesPayer("PRIMARYRECEIVER");
-//            payRequest.setCancelUrl("http://sounds.sg/payment-cancelled.jsp");//Return if payment cancelled
-//            payRequest.setReturnUrl("http://sounds.sg/MusicManagementController?target=CompletePayment&paymentID=" + payment.getId() + "&UUID=" + payment.getUUID());//Return after payment complete
-            payRequest.setCancelUrl("http://localhost:8080/DanielMusic-war/payment-cancelled.jsp");//Return if payment cancelled
-            payRequest.setReturnUrl("http://localhost:8080/DanielMusic-war/MusicManagementController?target=CompletePayment&paymentID=" + payment.getId() + "&UUID=" + payment.getUUID());//Return after payment complete
+            payRequest.setCancelUrl("http://sounds.sg/payment-cancelled.jsp");//Return if payment cancelled
+            payRequest.setReturnUrl("http://sounds.sg/MusicManagementController?target=CompletePayment&paymentID=" + payment.getId() + "&UUID=" + payment.getUUID());//Return after payment complete
+//            payRequest.setCancelUrl("http://localhost:8080/DanielMusic-war/payment-cancelled.jsp");//Return if payment cancelled
+//            payRequest.setReturnUrl("http://localhost:8080/DanielMusic-war/MusicManagementController?target=CompletePayment&paymentID=" + payment.getId() + "&UUID=" + payment.getUUID());//Return after payment complete
             payRequest.setCurrencyCode("SGD");
             //payRequest.setIpnNotificationUrl("http://replaceIpnUrl.com");
 
