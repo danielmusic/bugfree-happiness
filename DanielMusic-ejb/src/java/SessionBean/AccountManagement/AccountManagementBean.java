d97fa5ec13eb04b6ecc68c2f38d2659204000d91
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2016-01-04 13:52:56 +0800

    Update login msg

--- DanielMusic-ejb/src/java/SessionBean/AccountManagement/AccountManagementBean.java
+++ DanielMusic-ejb/src/java/SessionBean/AccountManagement/AccountManagementBean.java
@@ -284,7 +284,7 @@ public class AccountManagementBean implements AccountManagementBeanLocal {
             }
             generateAndSendVerificationEmail(result.getID(), email, false);
             result.setResult(true);
-            result.setDescription("Account registered successfully. You may now login");
+            result.setDescription("Your account has been successfully registered! Please log in below.");
             return result;
         } catch (Exception ex) {
             log.info("AccountManagementBean: registerAccount() failed");
